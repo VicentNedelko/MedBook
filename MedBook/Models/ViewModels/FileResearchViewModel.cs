e48c2f742e97a804ec160e34f43374433df683ed
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2021-08-03 21:53:02 +0300

    feat : add PDFConverter class library

---
+++ MedBook/Models/ViewModels/FileResearchViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedBook.Models.ViewModels
+{
+    public class FileResearchViewModel
+    {
+        public IFormFile PdfFile { get; set; }
+
+        [DisplayName("PDF")]
+        public string Pdf { get; set; }
+    }
+}
---
+++ PDFConverter/PdfGetter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PDFConverter
+{
+    public class PdfGetter
+    {
+        public static double GetParameterValue(string param)
+        {
+            double value = 0.0;
+            string[] elementsOfString = param.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var str in elementsOfString)
+            {
+                if (Double.TryParse(str, System.Globalization.NumberStyles.AllowDecimalPoint, System.Globalization.CultureInfo.InvariantCulture, out value))
+                {
+                    Console.WriteLine(value);
+                    break;
+                }
+            };
+            return value;
+        }
+    }
+}
