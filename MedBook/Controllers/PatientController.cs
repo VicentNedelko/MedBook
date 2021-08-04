5aaaaf7ac65a83249ef27abc4c541ba57f9df2d1
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2021-08-04 17:45:30 +0300

    feat : add Patient controller

---
+++ MedBook/Controllers/PatientController.cs
@@ -0,0 +1,54 @@
+﻿using MedBook.Models.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedBook.Controllers
+{
+    public class PatientController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public PatientController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+
+        [HttpGet]
+        public IActionResult ResearchUpload()
+        {
+            return View(new UploadFileVM());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ResearchUploadAsync(UploadFileVM model)
+        {
+            if(model.File != null)
+            {
+                var fileName = Path.GetFileName(model.File.FileName);
+                string ext = Path.GetExtension(model.File.FileName);
+                if(Path.GetExtension(model.File.FileName).ToUpper() != ".PDF")
+                {
+                    ViewBag.ErrorMessage = "File is NOT a PDF-file. Please, choose the required file.";
+                    return View();
+                }
+                var filePath = Path.Combine(_webHostEnvironment.WebRootPath, "uploads", fileName);
+
+                using(var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await model.File.CopyToAsync(fileStream);
+                }
+                ViewBag.InfoMessage = "File uploaded successfully.";
+                return View();
+            }
+            ViewBag.ErrorMessage = "File is empty.";
+            return View();
+        }
+    }
+}
